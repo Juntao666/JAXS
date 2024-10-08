db5c235002d3293feef681604e87e9f0b25bba01
xxiaoling <100384762+xxiaoling@users.noreply.github.com>
2024-10-08 18:44:43 -0400

    created people.py and test_people.py with retrieval function
    get_people
    test_get_people

---
+++ data/people.py
@@ -0,0 +1,38 @@
+"""
+This module interfaces to our user data.
+"""
+
+MIN_USER_NAME_LEN = 2
+# fields
+NAME = 'name'
+ROLES = 'roles'
+AFFILIATION = 'affiliation'
+EMAIL = 'email'
+
+TEST_EMAIL = 'netID@nyu.edu'
+DEL_EMAIL = 'delete@nyu.edu'
+
+people_dict = {
+    TEST_EMAIL: {
+        NAME: 'Person Created',
+        ROLES: [],
+        AFFILIATION: 'NYU',
+        EMAIL: TEST_EMAIL,
+    },
+    DEL_EMAIL: {
+        NAME: 'Person Deleted',
+        ROLES: [],
+        AFFILIATION: 'NYU',
+        EMAIL: DEL_EMAIL,
+    },
+}
+
+def get_people():
+    """
+        PARAM: none
+        RET: a dictionary of users emails as keys
+
+        Note: each user email must be the key for another dictionary
+    """
+    people = people_dict
+    return people
---
+++ data/tests/test_people.py
@@ -0,0 +1,13 @@
+# import pytest
+
+import data.people as ppl
+
+
+def test_get_people():
+    people = ppl.get_people()
+    assert isinstance(people, dict)
+    assert len(people) > 0
+    # check for string IDs:
+    for _id, person in people.items():
+        assert isinstance(_id, str)
+        assert ppl.NAME in person
