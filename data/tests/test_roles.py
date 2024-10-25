dcbc54d4341d4531b896f283d927341706fe922b
Juntao666 <jl13036@nyu.edu>
2024-10-24 21:17:26 -0400

    Update test_roles.py

--- data/tests/test_roles.py
+++ data/tests/test_roles.py
@@ -9,9 +9,18 @@ def test_get_roles():
         assert isinstance(code, str)
         assert isinstance(role, str)
 
+
 def test_get_masthead_roles():
     mh_roles = rls.get_masthead_roles()
     assert isinstance(mh_roles, dict)
+
+
+def test_get_role_codes():
+    codes = rls.get_role_codes()
+    assert isinstance(codes, list)
+    for code in codes:
+        assert isinstance(code, str)
+
 
 def test_is_valid():
     assert rls.is_valid(rls.TEST_CODE)
