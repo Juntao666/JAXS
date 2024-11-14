ecb0871a4ed75cc459e0596c105e49c987f6cf64
AnnaXT <annateng8000@gmail.com>
2024-11-14 17:20:32 -0500

    Added new files and tests for manuscript fields

--- data/manuscripts/tests/test_fields.py
+++ data/manuscripts/tests/test_fields.py
@@ -1,5 +1,25 @@
 import data.manuscripts.fields as mflds
 
+from unittest.mock import patch
+
 
 def test_get_flds():
     assert isinstance(mflds.get_flds(), dict)
+
+
+def test_get_fld_names():
+    assert isinstance(mflds.get_fld_names(), list)
+
+
+@patch('data.manuscripts.fields.get_disp_name', autospec=True,
+       return_value="Display_name")
+def test_get_disp_name(mock_read):
+    result = mflds.get_disp_name("title")
+    assert isinstance(result, str)
+
+
+@patch('data.manuscripts.fields.get_disp_name', autospec=True,
+       return_value=None)
+def test_get_disp_name_not_exist(mock_read):
+    result = mflds.get_disp_name("none_existant")
+    assert result is None
